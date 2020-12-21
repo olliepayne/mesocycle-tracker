4c9635128c23cefc2f972fee22a0fa7e700de7e2
Oliver Payne <olliepayne@me.com>
2020-12-20 20:18:03 -0700

    backup

--- routes/auth.js
+++ routes/auth.js
@@ -2,24 +2,20 @@ const router = require('express').Router();
 const passport = require('passport');
 
 // Google OAuth login route
-router.get(
-  "/google", 
-  passport.authenticate(
-    "google", {scope: ["profile", "email"]}
-  )
+router.get("/google", passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
 );
 
 // Google OAuth callback route
-router.get(
-  "/google/oauth2callback",
-  passport.authenticate("google", {
+router.get("/google/oauth2callback", passport.authenticate("google", {
     successRedirect: "/users",
     failureRedirect: "/users",
   })
 );
 
 // OAuth logout route
-router.get("/logout", function (req, res) {
+router.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/users");
 });
