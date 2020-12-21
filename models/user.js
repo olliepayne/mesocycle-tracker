7f0d406238504046f3ccb4bccb99ba86cfaeef57
Oliver Payne <olliepayne@me.com>
2020-12-21 13:32:45 -0700

    backup

--- models/user.js
+++ models/user.js
@@ -3,9 +3,13 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: String,
+  alias: String,
   email: String,
+  avatar: String,
+  googleId: String,
+  bio: String,
+  friends: [{type: Schema.Types.ObjectId, ref: "User"}],
   mesocycles: [{type: Schema.Types.ObjectId, ref: 'Mesocycle'}],
-  googleID: String
 });
 
 module.exports = mongoose.model('User', userSchema);
