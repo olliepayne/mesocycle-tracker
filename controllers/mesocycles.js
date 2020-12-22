8619e394559d193c1572ec6c6ddee12f3b707b9a
Oliver Payne <olliepayne@me.com>
2020-12-22 04:37:29 -0700

    backup

--- controllers/mesocycles.js
+++ controllers/mesocycles.js
@@ -26,12 +26,7 @@ function newOne(req, res) {
 }
 
 function create(req, res) {
-  const newMesocycle = new Mesocycle(req.body);
-  newMesocycle.save();
-  User.findById(req.params.id, (err, result) => {
-    result.mesocycles.push(newMesocycle);
-    res.redirect('/');
-  })
+  
 }
 
 function show(req, res) {
