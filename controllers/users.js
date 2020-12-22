1281e4ed9e7f439197c2d08ac36116005fe03e70
Oliver Payne <olliepayne@me.com>
2020-12-22 03:45:58 -0700

    backup

--- controllers/users.js
+++ controllers/users.js
@@ -10,5 +10,11 @@ function index(req, res) {
 }
 
 function show(req, res) {
-  
+  User.findById(req.params.id, (err, result) => {
+    res.render('users/show', {
+      title: 'Test',
+      user: req.user,
+      userModel: result
+    });
+  })
 }
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 const usersCtrl = require('../controllers/users');
 
-router.get('/', usersCtrl.index);
-router.get('/:id', usersCtrl.show);
+router.get('/', isLoggedIn, usersCtrl.index);
+router.get('/:id', isLoggedIn, usersCtrl.show);
+
+// Insert this middleware for routes that require a logged in user
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/auth/google");
+}
 
 module.exports = router;
