4bb413527aee5c7fb4c37efb1d3e39e0fc7f4fa9
Oliver Payne <olliepayne@me.com>
2021-01-03 14:30:59 -0700

    implement you page edit

--- controllers/users.js
+++ controllers/users.js
@@ -16,9 +16,16 @@ function show(req, res) {
   User.findById(req.user._id).
   populate('mesocycles'). 
   exec((err, user) => {
+    let canEdit = false;
+    if(req.params.id == req.user._id) {
+      canEdit = true;
+      console.log(canEdit);
+    }
+
     res.render('users/show', {
       title: `User ${user._id}`,
-      user
+      user,
+      canEdit
     });
   });
 }
