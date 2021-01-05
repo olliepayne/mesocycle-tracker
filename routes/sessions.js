66d0587d454272a459ba1b99a89a9565ea6ed737
Oliver Payne <olliepayne@me.com>
2021-01-05 13:50:04 -0700

    tweak user edit

--- routes/sessions.js
+++ routes/sessions.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const sessionCtrl = require('../controllers/sessions')
 
 router.get('/', sessionCtrl.index)
-router.get('/:id', sessionCtrl.show)
+router.get('/:sid', sessionCtrl.show)
 router.get('/', sessionCtrl.new)
 router.post('/', sessionCtrl.create)
 
